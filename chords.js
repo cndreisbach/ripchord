ba2d55f4bfdca6788b3c535ca6f589127da7ccee
Clinton Dreisbach <clinton@dreisbach.us>
2019-07-09 14:47:43 -0400

    Add initial files

---
+++ chords.js
@@ -0,0 +1,19 @@
+const majorProgressions = {
+  "I": ["ii", "iii", "IV", "V", "vi", "vii"],
+  "ii": ["V", "vii"],
+  "iii": ["vi"],
+  "IV": ["V", "vii"],
+  "V": ["I"],
+  "vi": ["ii", "IV"],
+  "vii": ["I"]
+}
+
+const minorProgressions = {
+  "i": ["ii", "III", "iv", "V", "VI", "vii"],
+  "ii": ["V", "vii"],
+  "III": ["VI"],
+  "iv": ["V", "vii"],
+  "V": ["i"],
+  "VI": ["ii", "iv"],
+  "vii": ["i"]
+}
