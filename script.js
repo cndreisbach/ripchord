c5887696babd9ae0642ffa5e493e78b37141863e
Clinton Dreisbach <clinton@dreisbach.us>
2019-07-22 17:12:21 -0400

    Add play button - non-functional

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
 import { majorProgressions, ChordProgressionGenerator, toTonalRoman } from './chords.js'
 import { fromRomanNumerals } from './web_modules/@tonaljs/progression.js'
-
+import { chord } from './web_modules/@tonaljs/chord.js'
 import Vue from './node_modules/vue/dist/vue.esm.browser.js'
+
+function convertProgressionToKey (progression, key) {
+  return fromRomanNumerals(key, progression)
+}
 
 const ChordWidget = new Vue({
   el: '#chord-widget',
@@ -14,9 +18,9 @@ const ChordWidget = new Vue({
   computed: {
     progressionsInKey: function () {
       if (this.key === '-') {
-        return this.progressions.map(p => p.join("-"))
+        return this.progressions.map(p => p.join('-'))
       } else {
-        return this.progressions.map(p => fromRomanNumerals(this.key, p).join("-"))
+        return this.progressions.map(p => convertProgressionToKey(p, this.key).join('-'))
       }
     }
   },
@@ -30,5 +34,10 @@ const ChordWidget = new Vue({
       // const chords = fromRomanNumerals(this.key, cg.history.map(toTonalRoman))
       this.progressions.push(cg.history.map(toTonalRoman))
     },
+
+    playProgression: function (progression) {
+      const chords = progression.split('-').map(p => chord(p))
+      console.log(chords)
+    }
   }
 })
